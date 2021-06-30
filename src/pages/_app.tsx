8dc0bf08b9c9a6332f8867c1b3de920a54370649
matheusmarks <matheus-marques@LAPTOP-K062DHUE.localdomain>
2021-06-30 20:20:13 -0300

    Finishing home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import '../styles/global.scss';
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+
+  );
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-import styles from '../styles/home.module.css';
+import Head from 'next/head';
+import styles from './home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World !!!</h1>
-  )
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>How about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
