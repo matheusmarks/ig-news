a54577f2d7a7f883404775951ace4798d2995913
matheusmarks <matheus-marques@LAPTOP-K062DHUE.localdomain>
2021-06-30 22:18:18 -0300

    Adding stripe as a payment method

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetServerSideProps } from 'next';
+import { stripe } from '../services/stripe';
 import Head from 'next/head';
 import styles from './home.module.scss';
 import { SubscribeButton } from '../components/SubscribeButton';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,7 +24,7 @@ export default function Home() {
           <h1>How about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9,90 month</span>
+            <span>for  {product.amount} month</span>
           </p>
 
           <SubscribeButton />
@@ -24,4 +33,25 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1J8CuUGCWDRe55YuNbG4Lpqr', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
